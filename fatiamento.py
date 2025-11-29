27683bdcd1f1f70afaebf10cd8fdadcd7e5ed2a5
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-11-29 11:01:38 -0300

    fatiamento e len

---
+++ fatiamento.py
@@ -0,0 +1,11 @@
+"""
+Fatiamento de strings 
+
+    012345678
+    olá mundo
+   -987654321 (negativo)
+   fatiamento[inicio:fim:passo]
+"""
+variavel = 'Olá mundo'
+print(variavel[4:5])
+print(variavel[-1:-10:-1])
