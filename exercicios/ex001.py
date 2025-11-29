387218211674f29941cb1dd57be456f2415c4163
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-11-29 11:26:10 -0300

    exercicios feito

---
+++ exercicios/ex001.py
@@ -0,0 +1,18 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    if idade.isalpha():
+        print('Idade deve ser um numero inteiro! Tente novamente.')
+    else:
+        print(f'Seu nome é {nome}')
+        print(f'Seu nome invertido é {nome[::-1]}')
+        if ' ' in nome:
+            print('Seu nome contem espaços')
+        else:
+            print('Seu nome não contem espaços') 
+        print(f'Seu nome tem {len(nome)} letras')
+        print(f'A primeira letra do seu nome é {nome[0]}')
+        print(f'A ultima letra do seu nome é {nome[-1]}')
+else:
+    print('Desculpe, você deixou campos vazios')
