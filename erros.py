56068596d57bb561aa87a3cfad82314dfb67c6bc
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-12-15 15:58:08 -0300

    finalizado

--- erros.py
+++ erros.py
@@ -2,13 +2,31 @@
     Introdução ao try/except
     try -> tenta executar o código
     except -> ocorreu algum erro ao tentar executar
+    finally -> sempre sera executado
+    raise -> lança exceções
+
+    link com os tipos de erros -> https://docs.python.org/pt-br/3/library/exceptions.html
+
 
 """
 
-numero_str = input('Vou dobrar o número que você digitar: ')
-
 try:
-    numero_float = float(numero_str)
-    print(f'O dobro do {numero_float} é {numero_float * 2}')
-except:
-    print('Isso não é um numero')
+    a = 18
+    b = 0
+    c = a / b
+except ZeroDivisionError: # O mais adequado é passar o tipo do erro que pode acontecer
+    print('Dividiu por zero')
+except NameError:
+    print('Variavel nao esta definida')
+except Exception as erro: # Todos os tipos de erros
+    print('Erro desconhecido')
+    print(erro)
+finally:
+    print('Obrigado por usar nosso sistema')
+
+idade = -5
+
+if idade < 0:
+    raise ValueError("Idade não pode ser negativa")
+
+
