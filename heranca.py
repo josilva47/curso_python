d6031029546ad25e42780ed682fa772f2acbfe92
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2026-01-12 12:07:17 -0300

    herança no python

---
+++ heranca.py
@@ -0,0 +1,21 @@
+
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar(self):
+        return f'Olá, meu nome é {self.nome} e meu sobrenome é {self.sobrenome}'
+
+
+class Cliente(Pessoa):
+    pass
+
+class Aluno(Pessoa):
+    ...
+
+c1 = Cliente('João', 'Araujo')
+a1 = Aluno('Rick', 'Almeida')
+
+print(c1.falar())
+print(a1.falar())
